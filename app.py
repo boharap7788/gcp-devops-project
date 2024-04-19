852b3bbd0e259b50e4ea3833ba1c46558dcf4fe8
Prakash Bohara <142651511+boharap7788@users.noreply.github.com>
2024-04-20 00:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+# ASCII art for Flask logo with project name and infinity sign
+flask_logo = '''
+      _ _       
+     | (_)      
+  ___| |_  __ _ 
+ / __| | |/ _` |
+ \__ \ | | (_| |
+ |___/_|_|\__,_|
+    GCP-DEVOPS-PROJECT
+            ∞
+'''
 
 @app.route('/')
 def hello_world():
-    return 'Hello,This is Simple Flask Application !!'
+    return flask_logo + '\n\nHello, This is a Simple Flask Application!!'
+
+@app.route('/app')
+def hello_app():
+    return flask_logo + '\n\nWelcome to the Application!!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
